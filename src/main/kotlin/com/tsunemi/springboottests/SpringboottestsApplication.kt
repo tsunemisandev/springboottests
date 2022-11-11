64b16247dca2e9df4eb231176beaa66b36f51668
“tsunemisandev <tsunemi.dev@gmail.com>
2022-11-11 21:21:15 +0900

    Initial commit

---
+++ src/main/kotlin/com/tsunemi/springboottests/SpringboottestsApplication.kt
@@ -0,0 +1,11 @@
+package com.tsunemi.springboottests
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringboottestsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringboottestsApplication>(*args)
+}
---
+++ src/test/kotlin/com/tsunemi/springboottests/SpringboottestsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tsunemi.springboottests
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringboottestsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
